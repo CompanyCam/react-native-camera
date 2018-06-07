27b4dc54409978bf70b84719d7c61bca09695bcf
Jason Gaare <jason.d.gaare@gmail.com>
2018-06-07 13:59:51 -0500

    remove overrides

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraPackage.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraPackage.java
@@ -12,17 +12,14 @@ import com.facebook.react.bridge.JavaScriptModule;
 
 public class RCTCameraPackage implements ReactPackage {
 
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
         return Collections.<NativeModule>singletonList(new RCTCameraModule(reactApplicationContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
         //noinspection ArraysAsListWithZeroOrOneArgument
         return Collections.<ViewManager>singletonList(new RCTCameraViewManager());
